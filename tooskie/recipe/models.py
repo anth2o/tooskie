aa4c5c6b966effba812ee9f2d6a71ffdf4d8cdec
Antoine <ahoorelbeke@gmail.com>
2018-09-19 11:31:05 +0200

    Removed migrations from git

--- tooskie/recipe/models.py
+++ tooskie/recipe/models.py
@@ -2,7 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from tooskie.user.models import BaseModel
 
-class Recipe(BaseModel):
-    name = models.CharField(max_length=1000, null=True, blank=True, verbose_name=_('Name'))
+class Recipe(models.Model):
+    name = models.CharField(max_length=1000, verbose_name=_('Name'))
     cooking_time = models.IntegerField(null=True, blank=True, verbose_name=_('Cooking time'))
     preparation_time = models.IntegerField(null=True, blank=True, verbose_name=_('Preparation time'))
