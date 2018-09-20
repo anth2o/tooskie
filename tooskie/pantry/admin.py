19b1200eb691cd2a3d8d11a03813b9f887c763df
Antoine <ahoorelbeke@gmail.com>
2018-09-20 17:06:00 +0200

    Add models in pantry app

---
+++ tooskie/pantry/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+import tooskie.pantry.models as models
+import sys, inspect
+
+for name, obj in inspect.getmembers(models):
+    if inspect.isclass(obj):
+        try:
+            admin.site.register(obj)
+        # Exception if the model is abstract
+        except:
+            pass
--- tooskie/pantry/models.py
+++ tooskie/pantry/models.py
@@ -11,10 +11,13 @@ from tooskie.constants import LINK_WORD
 
 
 class Pantry(NameModel):
+    class Meta:
+        verbose_name_plural = 'Pantries'
+
     has_freezer = models.NullBooleanField()
     has_fridge = models.NullBooleanField()
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
 
 class IngredientInPantry(BaseModel):
     quantity = models.FloatField(blank=True, null=True)
@@ -26,3 +29,14 @@ class IngredientInPantry(BaseModel):
     def __str__(self):
         return str(self.measure_of_ingredient) + LINK_WORD + str(self.pantry)
 
+class UstensilInPantry(BaseModel):
+    quantity = models.FloatField(blank=True, null=True)
+
+    # Relations
+    pantry = models.ForeignKey('Pantry', on_delete=models.CASCADE)
+    ustensil = models.ForeignKey('recipe.Ustensil', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.ustensil) + LINK_WORD + str(self.pantry)
+
+# TODO: implement models related to receipt (picture, list of ingredients, tricount...)
