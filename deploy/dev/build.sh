edd38111b8a03382a5622dfcebf2ba1fa8d08f1c
EC2 Default User <ec2-user@ip-172-31-45-54.eu-west-3.compute.internal>
2019-02-03 11:21:16 +0000

    Change script to install docker-compose

--- deploy/dev/build.sh
+++ deploy/dev/build.sh
@@ -4,6 +4,6 @@ sudo service docker start
 
 docker run hello-world
 
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo url -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
