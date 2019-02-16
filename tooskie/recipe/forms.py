3cbfbc2c52fb26085df9cb6370d894704c9a1954
Antoine <ahoorelbeke@gmail.com>
2019-02-16 16:01:26 +0100

    Minor improvements

--- tooskie/recipe/forms.py
+++ tooskie/recipe/forms.py
@@ -5,7 +5,7 @@ from .models import Recipe, Step
 RecipeSteps = inlineformset_factory(
                                 Recipe,
                                 Step,
-                                fields=('description',),
+                                fields=('description', 'time_start', 'picture'),
                                 extra=1,
                                 can_delete=False
                             )
