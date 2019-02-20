f31c573738ee8b72db4e10250d92bdd06f449517
Antoine <ahoorelbeke@gmail.com>
2019-02-20 18:18:31 +0100

    Remove exception

--- tooskie/recipe/forms.py
+++ tooskie/recipe/forms.py
@@ -29,20 +29,18 @@ class IngredientsForm(forms.Form):
     def save(self, recipe, *args, **kwargs):
         if not self.cleaned_data:
             return
-        try:
-            if self.cleaned_data['DELETE']:
-                self.delete(recipe, *args, **kwargs)
-                return
-            unit_of_ingredient, created = UnitOfIngredient.objects.get_or_create(
-                unit=self.cleaned_data['unit'], ingredient=self.cleaned_data['ingredient'])
-            if created:
-                unit_of_ingredient.save()
-            ingredient_in_recipe, created = IngredientInRecipe.objects.get_or_create(
-                unit_of_ingredient=unit_of_ingredient, recipe=recipe, quantity=self.cleaned_data['quantity'])
-            if created:
-                ingredient_in_recipe.save()
-        except Exception as e:
-            logger.error(e)
+        logger.debug(self.cleaned_data)
+        if self.cleaned_data['DELETE']:
+            self.delete(recipe, *args, **kwargs)
+            return
+        unit_of_ingredient, created = UnitOfIngredient.objects.get_or_create(
+            unit=self.cleaned_data['unit'], ingredient=self.cleaned_data['ingredient'])
+        if created:
+            unit_of_ingredient.save()
+        ingredient_in_recipe, created = IngredientInRecipe.objects.get_or_create(
+            unit_of_ingredient=unit_of_ingredient, recipe=recipe, quantity=self.cleaned_data['quantity'])
+        if created:
+            ingredient_in_recipe.save()
 
     def delete(self, recipe, *args, **kwargs):
         unit_of_ingredient, _ = UnitOfIngredient.objects.get_or_create(
@@ -69,16 +67,13 @@ class NutritionalPropertiesForm(forms.Form):
     def save(self, recipe, *args, **kwargs):
         if not self.cleaned_data:
             return
-        try:
-            if self.cleaned_data['DELETE']:
-                self.delete(recipe, *args, **kwargs)
-                return
-            recipe_has_nutritional_properties, created = RecipeHasNutritionalProperty.objects.get_or_create(
-                recipe=recipe, nutritional_property=self.cleaned_data['nutritional_property'], unit_of_nutritional_property=self.cleaned_data['unit'], quantity=self.cleaned_data['quantity'])
-            if created:
-                recipe_has_nutritional_properties.save()
-        except Exception as e:
-            logger.error(e)
+        if self.cleaned_data['DELETE']:
+            self.delete(recipe, *args, **kwargs)
+            return
+        recipe_has_nutritional_properties, created = RecipeHasNutritionalProperty.objects.get_or_create(
+            recipe=recipe, nutritional_property=self.cleaned_data['nutritional_property'], unit_of_nutritional_property=self.cleaned_data['unit'], quantity=self.cleaned_data['quantity'])
+        if created:
+            recipe_has_nutritional_properties.save()
     
     def delete(self, recipe, *args, **kwargs):
         recipe_has_nutritional_properties, _ = RecipeHasNutritionalProperty.objects.get_or_create(
@@ -97,14 +92,10 @@ class RecipeForm(forms.Form):
     def save(self, tag, *args, **kwargs):
         if not self.cleaned_data:
             return
-        try:
-            if self.cleaned_data['DELETE']:
-                self.delete(tag, *args, **kwargs)
-                return
-            logger.debug(self.cleaned_data)
-            self.cleaned_data['recipe'].tag.add(tag)
-        except Exception as e:
-            logger.error(e)
+        if self.cleaned_data['DELETE']:
+            self.delete(tag, *args, **kwargs)
+            return
+        self.cleaned_data['recipe'].tag.add(tag)
     
     def delete(self, tag, *args, **kwargs):
         self.cleaned_data['recipe'].tag.remove(tag)
